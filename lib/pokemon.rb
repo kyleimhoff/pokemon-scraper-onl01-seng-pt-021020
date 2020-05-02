9eae86db6741b38c76a2a63341c43218581689f9
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-02 18:32:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,4 +1,13 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :hp, :db 
+  
+  def initialize(id, name, type, hp, db)
+    @id = id 
+    @name = name 
+    @type = type 
+    @hp = hp 
+    @db = db 
+  end
   
   
 end
